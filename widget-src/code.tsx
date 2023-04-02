628b0c349504251c6285425c37987df17c904d09
Izuchukwu Elechi <izuchukwu@hey.com>
2023-04-02 05:21:44 -0700

    It works! WIP streaming

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,68 +1,3 @@
-// // This is a counter widget with buttons to increment and decrement the number.
-
-// const { widget } = figma
-// const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG } = widget
-
-// function Widget() {
-//   const [count, setCount] = useSyncedState('count', 0)
-
-//   if (count !== 0) {
-//     usePropertyMenu(
-//       [
-//         {
-//           itemType: 'action',
-//           propertyName: 'reset',
-//           tooltip: 'Reset',
-//           icon: `<svg width="22" height="15" viewBox="0 0 22 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-//           <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9026 1.43168C12.1936 1.47564 12.4822 1.54098 12.7663 1.62777L12.7719 1.62949C14.0176 2.0114 15.109 2.78567 15.8858 3.83854L15.8918 3.84665C16.5473 4.73808 16.9484 5.78867 17.058 6.88508L14.0863 4.88858L13.3259 6.02047L17.3852 8.74774L17.9079 9.09894L18.2994 8.60571L21.0056 5.19662L19.9376 4.34879L18.3531 6.34479C18.3424 6.27511 18.3306 6.20563 18.3179 6.13636C18.1135 5.02233 17.6601 3.96334 16.9851 3.04274L16.9791 3.03462C16.0303 1.74427 14.6956 0.794984 13.1714 0.326388L13.1658 0.32466C12.8171 0.217755 12.4627 0.137298 12.1055 0.0832198C10.899 -0.0994351 9.66061 0.0188515 8.50099 0.435448L8.4947 0.437711C7.42511 0.823053 6.46311 1.44778 5.6774 2.25801C5.38576 2.55876 5.11841 2.88506 4.87886 3.23416C4.85856 3.26376 4.83845 3.29351 4.81854 3.32343L5.94262 4.08294L5.94802 4.07484C5.96253 4.0531 5.97717 4.03146 5.99195 4.00993C6.71697 2.95331 7.75331 2.15199 8.95541 1.72013L8.9617 1.71788C9.33245 1.58514 9.71301 1.48966 10.098 1.43156C10.6957 1.34135 11.3039 1.34123 11.9026 1.43168ZM3.70034 6.39429L0.994141 9.80338L2.06217 10.6512L3.64663 8.65521C3.65741 8.72489 3.66916 8.79437 3.68187 8.86364C3.88627 9.97767 4.33964 11.0367 5.01467 11.9573L5.02063 11.9654C5.96945 13.2557 7.30418 14.205 8.82835 14.6736L8.83398 14.6753C9.18281 14.7823 9.53732 14.8628 9.89464 14.9168C11.101 15.0994 12.3393 14.9811 13.4988 14.5646L13.5051 14.5623C14.5747 14.1769 15.5367 13.5522 16.3224 12.742C16.614 12.4413 16.8813 12.115 17.1209 11.7659C17.1412 11.7363 17.1613 11.7065 17.1812 11.6766L16.0571 10.9171L16.0518 10.9252C16.0372 10.9469 16.0225 10.9686 16.0078 10.9902C15.2827 12.0467 14.2464 12.848 13.0444 13.2799L13.0381 13.2821C12.6673 13.4149 12.2868 13.5103 11.9018 13.5684C11.3041 13.6587 10.6958 13.6588 10.0971 13.5683C9.8062 13.5244 9.51754 13.459 9.23347 13.3722L9.22784 13.3705C7.98212 12.9886 6.89078 12.2143 6.11393 11.1615L6.10795 11.1534C5.45247 10.2619 5.05138 9.21133 4.94181 8.11492L7.91342 10.1114L8.6739 8.97953L4.61459 6.25226L4.09188 5.90106L3.70034 6.39429Z" fill="white"/>
-//           </svg>
-//           `,
-//         },
-//       ],
-//       () => {
-//         setCount(0)
-//       },
-//     )
-//   }
-
-//   return (
-//     <AutoLayout
-//       verticalAlignItems={'center'}
-//       spacing={8}
-//       padding={16}
-//       cornerRadius={8}
-//       fill={'#FFFFFF'}
-//       stroke={'#E6E6E6'}
-//     >
-//       <SVG
-//         src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <rect width="30" height="30" rx="15" fill="white"/>
-//         <rect x="7.5" y="14.0625" width="15" height="1.875" fill="black" fill-opacity="0.8"/>
-//         <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
-//         </svg>`}
-//         onClick={() => {
-//           setCount(count - 1)
-//         }}
-//       ></SVG>
-//       <Text fontSize={32} width={42} horizontalAlignText={'center'}>
-//         {count}
-//       </Text>
-//       <SVG
-//         src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <rect width="30" height="30" rx="15" fill="white"/>
-//         <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
-//         <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
-//         </svg>`}
-//         onClick={() => {
-//           setCount(count + 1)
-//         }}
-//       ></SVG>
-//     </AutoLayout>
-//   )
-// }
-
-// widget.register(Widget)
-
 const {widget} = figma
 const {
 	AutoLayout,
@@ -102,11 +37,6 @@ function FableChat() {
 		content: string
 	}>('systemMessage', {role: 'system', content: ''})
 
-	const [costVisible, setCostVisible] = useSyncedState<boolean>(
-		'costVisible',
-		false
-	)
-
 	const [keyVisible, setKeyVisible] = useSyncedState<boolean>(
 		'keyVisible',
 		false
@@ -115,6 +45,14 @@ function FableChat() {
 		role: 'OpenAI Key'
 		content: string
 	}>('key', {role: 'OpenAI Key', content: ''})
+
+	const [loadState, setLoadState] = useSyncedState<
+		'ready' | 'loading' | 'error'
+	>('loadState', 'ready')
+	const [error, setError] = useSyncedState<Record<string, string>>(
+		'error',
+		{}
+	)
 
 	usePropertyMenu(
 		[
@@ -265,13 +203,6 @@ function FableChat() {
 				isToggled: keyVisible,
 				icon: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="m21 2-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0 3 3L22 7l-3-3m-3.5 3.5L19 4"></path></svg>`
 			},
-			{
-				propertyName: 'toggleCost',
-				itemType: 'toggle',
-				tooltip: 'Toggle Tokens & Cost',
-				isToggled: costVisible,
-				icon: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="2" x2="12" y2="22"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>`
-			},
 
 			{
 				propertyName: 'resetChat',
@@ -287,9 +218,6 @@ function FableChat() {
 					break
 				case 'toggleSystem':
 					setSystemMessageVisible((v) => !v)
-					break
-				case 'toggleCost':
-					setCostVisible((v) => !v)
 					break
 				case 'toggleKey':
 					setKeyVisible((v) => !v)
@@ -315,25 +243,56 @@ function FableChat() {
 	}
 
 	const submit = async () => {
+		setLoadState('loading')
 		waitForTask(
 			new Promise((resolve) => {
 				figma.showUI(__html__, {visible: false})
 				figma.ui.postMessage({
 					type: 'submit',
-					messages,
-					systemMessage,
+					messages: systemMessage.content
+						? [systemMessage, ...messages]
+						: messages,
 					key: keyMessage.content
 				})
 
-				figma.ui.onmessage = async (
-					response: {role: string; content: string}[]
-				) => {
-					// 	newMessages.forEach(msg => {
-					// 		messages.set()
-					//   messages.set(msg);
-					console.log(response)
-					resolve(true)
+				figma.ui.onmessage = async (response: {
+					assistantMessage?: string
+					error?: Record<string, string>
+				}) => {
+					if (response.error) {
+						setLoadState('error')
+						setError(response.error)
+					} else if (response.assistantMessage) {
+						const newMessages = [
+							...messages,
+							{
+								role: 'assistant',
+								content: response.assistantMessage
+							}
+						]
+						setMessages(newMessages)
+						setLoadState('ready')
+					}
+					// @ts-ignore
+					resolve()
 				}
+
+				// STREAMING
+				// figma.ui.onmessage = async (response: {
+				// 	messages?: {role: string; content: string}[]
+				// 	error?: Record<string, string>
+				// 	state?: 'streaming' | 'complete'
+				// }) => {
+				// 	if (response.error) {
+				// 		setLoadState('error')
+				// 		setError(response.error)
+				// 	} else if (response.messages) {
+				// 		setMessages(response.messages)
+				// 		if (response.state === 'complete') setLoadState('ready')
+				// 	}
+				// 	// @ts-ignore
+				// 	resolve()
+				// }
 			})
 		)
 	}
@@ -465,6 +424,8 @@ function FableChat() {
 							<Input
 								width="fill-parent"
 								value={message.content}
+								fontWeight={400}
+								lineHeight={23}
 								onTextEditEnd={(e) => {
 									// If msg is system or key, set appropriate state
 									if (message.role === 'system') {
@@ -605,6 +566,55 @@ function FableChat() {
 					</Text>
 				</AutoLayout>
 			</AutoLayout>
+			{loadState === 'error' && (
+				<AutoLayout
+					direction="vertical"
+					spacing={8}
+					padding={{vertical: 16, bottom: 10, horizontal: 8}}
+					cornerRadius={4}
+					width="fill-parent"
+					horizontalAlignItems={'center'}
+					stroke="#ff0000"
+				>
+					<SVG
+						src={`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>`}
+					/>
+					<Text
+						fontFamily="Inter"
+						fontSize={13}
+						letterSpacing={0.1}
+						horizontalAlignText="center"
+						verticalAlignText="center"
+						fill="#ff0000"
+						width={360}
+					>
+						{error.message}
+					</Text>
+					<AutoLayout
+						direction="horizontal"
+						spacing={8}
+						padding={{vertical: 6, horizontal: 8}}
+						cornerRadius={4}
+						hoverStyle={{
+							fill: '#FFE5E5'
+						}}
+						onClick={() => setLoadState('ready')}
+					>
+						<Text
+							fontFamily="Inter"
+							fontSize={13}
+							letterSpacing={0.1}
+							horizontalAlignText="left"
+							verticalAlignText="center"
+							fill="#ff0000"
+							height={20}
+							fontWeight={600}
+						>
+							OK
+						</Text>
+					</AutoLayout>
+				</AutoLayout>
+			)}
 		</AutoLayout>
 	)
 }
